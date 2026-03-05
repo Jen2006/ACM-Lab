05dd1fed348889a071e1940598b4d184eca9b225
Jenisha Castelino <jenishacastelino8@gmail.com>
2026-03-05 10:21:39 +0530

    added provider file

---
+++ provider.ps1
@@ -0,0 +1,10 @@
+# List all providers
+# Get-PSProvider
+
+# Access Registry
+# Set-Location HKCU:\Software
+# Get-ChildItem
+
+# # Access Environment Variables
+Set-Location Env:
+Get-ChildItem
--- verbose.ps1
+++ verbose.ps1
@@ -1,1 +1,2 @@
+#Verbose lets uk wht the command is doing or gives you the detailed explantion of the commands used
 New-Item -Path "C:\Temp\VerboseFolder" -ItemType Directory -Verbose
