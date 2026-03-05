7357579bbce5eb29f6fc58786179e79816d77479
Jenisha Castelino <jenishacastelino8@gmail.com>
2026-03-05 10:52:01 +0530

    added  file

---
+++ Get.ps1
@@ -0,0 +1,13 @@
+#Get-ChildItem "C:\Users\jenis\OneDrive\Desktop\Automation-Configuration" 
+
+
+#Get-Location
+
+
+# Create file and add content
+#New-Item -Path "C:\Temp\sample.txt" -ItemType File
+# Add-Content "C:\Temp\sample.txt" "Hello World"
+# Add-Content "C:\Temp\sample.txt" "PowerShell Lab"
+# # # Read file
+# Get-Content "C:\Temp\sample.txt"
+
